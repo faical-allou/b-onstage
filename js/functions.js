3649c20c691ae91a9f5fb2190e68de4054282207
server <support@b-onstage.com>
2013-08-01 19:47:07 +0200

    Fix contact form bug

--- js/functions.js
+++ js/functions.js
@@ -164,7 +164,7 @@ function init_footer(){
 							selectedList	: 1,
 							height			: 'auto'							
 						});							
-						$('#form-contact input:text, #form-contact textarea').fieldWidth(1.0);
+						//$('#form-contact input:text, #form-contact textarea').fieldWidth(1.0);
 						$('#form-contact').validate({
 							submitHandler: function(form) {																					
 								$.ajax({
