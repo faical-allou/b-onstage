9ad69281492daed4dfc02de23c225e9178ef8ec7
server <support@b-onstage.com>
2013-08-01 19:49:19 +0200

    Fix forgot psw form bug

--- application/views/user/forgot_password.php
+++ application/views/user/forgot_password.php
@@ -1,14 +1,14 @@
-<h1>Forgot Password</h1>
-<p>Please enter your email address so we can send you an email to reset your password.</p>
+<h1><?php echo lang("forgot_header") ?></h1>
+<p><?php echo lang("forgot_txt") ?></p>
 
 <div id="infoMessage"><?php echo $message;?></div>
 
 <?php echo form_open("user/forgot_password");?>
 
-      <p>Email Address:<br />
+      <p><?php echo lang("identity") ?>:<br />
       <?php echo form_input($email);?>
       </p>
       
-      <p><?php echo form_submit('submit', 'Submit');?></p>
+      <p><?php echo form_submit('submit', lang("submit"));?></p>
       
 <?php echo form_close();?>
