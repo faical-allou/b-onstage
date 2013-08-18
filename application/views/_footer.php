5cd6913967626a2fd1f1cf4f946607a6331aaa97
server <support@b-onstage.com>
2013-08-18 13:23:11 +0200

    Add Google Analytics

--- application/views/_footer.php
+++ application/views/_footer.php
@@ -59,5 +59,15 @@
 		<?php if(isset($scripts)){ foreach($scripts as $script){ ?>
 		<script src="<?=site_url($script)?>"></script>
 		<?php }} ?>
+		<script type="text/javascript">
+		  var _gaq = _gaq || [];
+		  _gaq.push(['_setAccount', 'UA-43278209-1']);
+		  _gaq.push(['_trackPageview']);
+		  (function() {
+		    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		  })();
+		</script>
 	</body>
 </html>
