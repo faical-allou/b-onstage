314afdd92cc39f85a3bfeb9826d5f8e27c0b72a7
Faical Allou <faical.allou@mybandonstage.com>
2015-02-26 23:27:12 +0100

    Improved the homepage display for screens 1024px and higher

--- application/views/_header.php
+++ application/views/_header.php
@@ -14,7 +14,7 @@ if($this->session->userdata('lang_loaded') == $value["name"]){ $lang_id = $value
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	<title><?=$title?></title>
 	<meta name="description" content="<?=$description?>">	
-	<meta name="viewport" content="width = device-width, initial-scale = 1.0" />
+	<meta name="viewport" content="width = device-width, initial-scale = 0.9" />
 	<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
 
 	<!--css-->
