a76358275fc2dad4990705b8b84e8bbc57684ccf
Faical Allou <faical.allou@mybandonstage.com>
2015-04-18 12:09:02 +0200

    Improved emails

--- application/views/user/email/delete_reservation.php
+++ application/views/user/email/delete_reservation.php
@@ -1,7 +1,7 @@
 <html>
 
 <body>
-<div style="width:device-width; initial-scale=1; text-align: justify; font-family: arial; font-size: 16px;background: #676767;">
+<div style="width:device-width; initial-scale=1; padding: 20px; text-align: justify; font-family: arial; font-size: 16px;background: #676767;">
     <div style="max-width: 400px;display: block; margin: 10px auto; padding: 20px; border: 1px; background: #ffffff;text-align: center;">
         <div style="text-align: center;"> 
     		<img src="https://gallery.mailchimp.com/6f434fa5389ea8c21ee610a76/images/7e761c53-7542-496f-b9b7-977f04aaf2c1.png" width="240" style="margin: auto;display: block;">
@@ -14,7 +14,7 @@
 	<p style="font-size:15px;font-family:'Arial',sans-serif;color:#3a3a3a;"><strong><?php echo lang("users_rese_refuse_email_txt2") ?> : </strong><?=$reservation_id?></p>
 	<p style="font-size:15px;font-family:'Arial',sans-serif;color:#3a3a3a;"><strong><?php echo lang("users_calendar_create_location") ?> : </strong><?=$stage_name?></p>
 	<p style="font-size:15px;font-family:'Arial',sans-serif;color:#3a3a3a;"><strong><?php echo lang("date") ?> : </strong><?=$event_date?></p>		
-	<p style="font-size:15px;font-family:'Arial',sans-serif;color:#3a3a3a;font-weight:bold;"><?=anchor($url_new_reservation, ucfirst (lang("header_myprofile")).' '.lang("users_rese_refuse_email_txt5"),array('style'=>'color:#8e2c86;'))?></p>		
+	<p style="font-size:15px;font-family:'Arial',sans-serif;color:#3a3a3a;font-weight:bold;"><?=anchor($url_new_reservation, ucfirst (lang("users_rese_refuse_email_txt5")),array('style'=>'color:#8e2c86;'))?></p>		
 	<p style="font-size:15px;font-family:'Arial',sans-serif;color:#3a3a3a;"><?php echo lang("users_rese_refuse_email_txt3") ?> <?=anchor($url_profil,lang("clickhere"),array('style'=>'color:#8e2c86;'))?></p>
 		</div>
 		
--- application/views/user/email/send_reservation.php
+++ application/views/user/email/send_reservation.php
@@ -1,6 +1,6 @@
 <html>
 <body>
-	<div style="width:device-width; initial-scale=1; text-align: justify; font-family: arial; font-size: 16px;background: #676767;">
+	<div style="width:device-width; initial-scale=1;padding: 20px; text-align: justify; font-family: arial; font-size: 16px;background: #676767;">
       <div style="max-width: 400px;display: block; margin: 10px auto; padding: 20px; border: 1px; background: #ffffff;text-align: center;">
         <div style="text-align: center;"> 
     		<img src="https://gallery.mailchimp.com/6f434fa5389ea8c21ee610a76/images/7e761c53-7542-496f-b9b7-977f04aaf2c1.png" width="240" style="margin: auto;display: block;">
