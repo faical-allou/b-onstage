4cb165da2d43fd732609ad0ee23257ed432ceb23
Faical Allou <faical.allou@mybandonstage.com>
2015-05-26 19:55:46 +0200

    typo

--- js/concert.js
+++ js/concert.js
@@ -328,7 +328,7 @@
 														.attr('href', '/user/reservations')
 														.addClass('show-reservation ui-purple')
 														.html('Voir ma réservation')
-														.button();
+														.button()
 														.window.location.reload();
 														break;
 													case 'ERROR'	: 
