37e71f166be4d900048baecb452b1a1e3bf80ebd
Faical Allou <faical.allou@mybandonstage.com>
2015-06-16 17:23:46 +0200

    corrected display for stages legend for narrow screens

--- application/views/stage/index.php
+++ application/views/stage/index.php
@@ -229,53 +229,53 @@
 		<!--legend-->
 		<div class="recommendations mb-10 mt-10">
 					<span class="dt">  <?php echo lang("legend") ?> </span>
-					<div class="dt">
+					<div class="dt fs-10">
 						<div class="dtc ta-c">
 							<div class="p-5">
 								<i class="fa fa-users"></i>
-								<p class="grey fs-12"><?php echo lang("room_size_legend") ?></p>
+								<p class="grey"><?php echo lang("room_size_legend") ?></p>
 							</div>	
 						</div>
 						
 						<div class="dtc ta-c">
 							<div class="p-5">
 								<i class="fa fa-square-o"></i>
-								<p class="grey fs-12"><?php echo lang("stage_size_legend") ?></p>
+								<p class="grey"><?php echo lang("stage_size_legend") ?></p>
 							</div>	
 						</div>
 						
 						<div class="dtc ta-c">
 							<div class="p-5">
 								<i class="fa fa-microphone"></i>
-								<p class="grey fs-12"><?php echo lang("microphone_legend") ?></p>
+								<p class="grey"><?php echo lang("microphone_legend") ?></p>
 							</div>	
 						</div>
 						
 						<div class="dtc ta-c">
 							<div class="p-5">
 								<i class="fa fa-volume-off"></i>
-								<p class="grey fs-12"><?php echo lang("speakers_legend") ?></p>
+								<p class="grey"><?php echo lang("speakers_legend") ?></p>
 							</div>	
 						</div>
 						
 						<div class="dtc ta-c">
 							<div class="p-5">
 								<i class="fa fa-sliders"></i>
-								<p class="grey fs-12"><?php echo lang("amplification_legend") ?></p>
+								<p class="grey"><?php echo lang("amplification_legend") ?></p>
 							</div>	
 						</div>
 						
 						<div class="dtc ta-c">
 							<div class="p-5">
 								<i class="fa fa-lightbulb-o"></i>
-								<p class="grey fs-12"><?php echo lang("lights_legend") ?></p>
+								<p class="grey "><?php echo lang("lights_legend") ?></p>
 							</div>	
 						</div>
 						
 						<div class="dtc ta-c">
 							<div class="p-5">
 								<i class="fa fa-bell-slash"></i>
-								<p class="grey fs-12"><?php echo lang("noise_legend") ?></p>
+								<p class="grey"><?php echo lang("noise_legend") ?></p>
 							</div>	
 						</div>
 						
