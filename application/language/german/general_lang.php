e9e768b56b354545b8f8213b62293dd2903f74f6
Faical Allou <faical.allou@mybandonstage.com>
2015-06-16 18:03:09 +0200

    added target_blank to link to survey

--- application/language/german/general_lang.php
+++ application/language/german/general_lang.php
@@ -1027,7 +1027,7 @@ $lang['book_req_success'] = 'Deine Anfrage wurde abgesandt.';
 // Notifications		
 $lang['notifs_1'] = 'Deine Anfrage wurde abgesandt.';
 $lang['notifs_2'] = 'Du hast eine Anfrage erhalten';
-$lang['notifs_3'] = 'Willkommen bei b-onstage: Was denkst du darüber? <a href=https://www.surveymonkey.com/s/QTSBNGZ>Kurze Umfrage</a>';
+$lang['notifs_3'] = 'Willkommen bei b-onstage: Was denkst du darüber? <a href=https://www.surveymonkey.com/s/QTSBNGZ target="_blank">Kurze Umfrage</a>';
 $lang['notifs_4'] = 'hat Deine Anfrage angenommen.';
 
 
